04afe283195ba5097196a22da834bde704f1c22d
linhphambuzz <mylinh.phamtran@gmail.com>
2025-03-28 10:35:48 -0500

    export host_ip to entrypoint script instead of setting env var

--- src/get_self_ip.py
+++ src/get_self_ip.py
@@ -5,18 +5,11 @@ from bosdyn.client.common import get_self_ip
 
 spot_host=os.getenv("spot_host")
 
-
-
 if __name__=="__main__":
 	
    host_ip=get_self_ip(spot_host)
-   path=os.path.dirname(__file__)
-   path_to_env=os.path.join(path,"../.devcontainer/.env")
-   with open(path_to_env,"w") as f:
-      to_be_written=f"host_ip={host_ip}"
-      f.write(to_be_written)
-   
-   assert open(path_to_env,"r").read()!=None, "host ip is not provided"
+   assert host_ip!=None, "host ip is NULL"
+   print(host_ip)
     
 
    
